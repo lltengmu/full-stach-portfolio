b1d6989d6f8eb3311db7cc1504399257275245c6
eddie <1150970484@qq.com>
2024-10-20 22:04:01 +0800

    remove auth module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { JWTModule } from './jwt/jwt.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, StudentModule, JWTModule],
+  imports: [PrismaModule, StudentModule, JWTModule],
   providers: [],
 })
 export class AppModule {}
